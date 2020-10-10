5746a48909f8ac4bff50bf4611c3ec12b689a802
AlysonBee <alyson.stars@yahoo.com>
2020-10-10 15:23:42 +0200

    adding error_list

--- src/error_list.c
+++ src/error_list.c
@@ -5,60 +5,57 @@
 
 t_token	*panic_mode(t_token *trav)
 {
-
 	if (strcmp(trav->type, "ID") == 0 || strcmp(trav->type, "NUM") == 0 || 
 		strcmp(trav->type, "LITERAL") == 0 || strcmp(trav->type, "CHAR") == 0)
        	{
-		printf("here\n");
 		if (stack && stack->scope_name == FOR)
-                                {
-                                        error = NULL;
-                                        trav = error_recover(trav, "Error: missing semilcolon",
-                                                push_token(error, ";", "SEMICOLON", trav->line, "NULL"));
-                                }
-                                else if (brackets != 0)
-                                {
-
-                                        error = NULL;
-                                        trav = forward_recovery(trav, "Error : missing a closing '}'",
-                                                push_token(error, "}", "CLOSINGBRACKET", trav->line, "NULL"));
-                                        back = trav;
-                                        prev = strdup(trav->name);
-                                }
-                                else
-                                {
-                                        error = NULL;
-                                        trav = forward_recovery(trav, "Error : missing semicolon",
-                                                push_token(error, ";", "SEMICOLON", trav->line, "NULL"));
-                                        back = trav;
-                                        prev = strdup(trav->name);
-                                }
-                        }
-                        else if (strcmp(trav->name, "}") == 0)
-                        {
-                                error = NULL;
-                                trav = forward_recovery(trav, "Error: missing semicolon",
-                                        push_token(error, ";", "SEMICOLON", trav->line, "NULL"));
-                                back = trav;
-                                prev = strdup(trav->name);
-                        }
-                        else if (strcmp(trav->name, ",") == 0)
-                        {
-                                if (strcmp(back->type, "CHAR") == 0)
-                                {
-                                        error = NULL;
-                                        trav = forward_recovery(trav, "Error: Incomplete list",
-                                                push_token(error, "'C'", back->type, trav->line, "NULL"));
-                                        back = trav;
-                                        prev = strdup(trav->name);
-                                }
-                        }
-                        else if (value_found(trav->name, start) == true)
-                        {
-                                error = NULL;
-                                trav = forward_recovery(trav, "Error: variable missing",
-                                        push_token(error, "X", "ID", trav->line, "NULL"));
-                                back = trav;
-                                prev = strdup(trav->name);
-                        }
+                {
+               		error = NULL;
+                       	trav = error_recover(trav, "Error: missing semilcolon",
+                        	push_token(error, ";", "SEMICOLON", trav->line, "NULL"));
+                }
+                else if (brackets != 0)
+                {
+                       	error = NULL;
+                        trav = forward_recovery(trav, "Error : missing a closing '}'",
+                        	push_token(error, "}", "CLOSINGBRACKET", trav->line, "NULL"));
+                        back = trav;
+                        prev = strdup(trav->name);
+                 }
+                 else
+                 {
+                 	error = NULL;
+                        trav = forward_recovery(trav, "Error : missing semicolon",
+                        	push_token(error, ";", "SEMICOLON", trav->line, "NULL"));
+                        back = trav;
+                        prev = strdup(trav->name);
+                }
+          }
+          else if (strcmp(trav->name, "}") == 0)
+          {
+          	error = NULL;
+          	trav = forward_recovery(trav, "Error: missing semicolon",
+                	push_token(error, ";", "SEMICOLON", trav->line, "NULL"));
+                back = trav;
+               	prev = strdup(trav->name);
+          }
+          else if (strcmp(trav->name, ",") == 0)
+          {
+          	if (strcmp(back->type, "CHAR") == 0)
+                {
+                	error = NULL;
+                        trav = forward_recovery(trav, "Error: Incomplete list",
+                                push_token(error, "'C'", back->type, trav->line, "NULL"));
+                        back = trav;
+                        prev = strdup(trav->name);
+                }
+          }
+          else if (value_found(trav->name, start) == true)
+          {
+          	error = NULL;
+                trav = forward_recovery(trav, "Error: variable missing",
+                        push_token(error, "X", "ID", trav->line, "NULL"));
+                back = trav;
+                prev = strdup(trav->name);
+          }
 }
